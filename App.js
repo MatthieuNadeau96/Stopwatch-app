e33cee0746bc972c35a8b17d2721ad39be1093ed
Matthieu <matthieunadeau96@gmail.com>
2019-01-16 10:36:54 -0500

    Implemented the Timer function, added some styling to it, & imported moment.js

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import React, {Component} from 'react'
 import {Platform, StyleSheet, Text, View} from 'react-native'
+import moment from 'moment'
 
 const DATA = {
   timer: 1234567,
   laps: [ 12234, 2345, 5555, 90009]
 }
 
-type Props = {};
+function Timer({ interval }) {
+  const duration = moment.duration(interval)
+  const centiseconds = Math.floor(duration.milliseconds() / 10)
+  return (
+    <Text style={styles.timer}>
+      {duration.minutes()}:{duration.seconds()}.{centiseconds}
+    </Text>
+  )
+}
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-
+        <Timer interval={DATA.timer}/>
       </View>
     )
   }
@@ -22,5 +31,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     backgroundColor: '#1f1f1f',
+    paddingTop: 130,
+  },
+  timer: {
+    color: '#ffffff',
+    fontSize: 76,
+    fontWeight: '200',
   }
 })
